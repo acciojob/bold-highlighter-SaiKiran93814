2273b7be216071ced22cdc6cddc901b4e3945cee
SaiKiran93814 <60767499+SaiKiran93814@users.noreply.github.com>
2025-05-21 22:32:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function highlight() {
     //Write your code here
+  const boldWords = document.querySelectorAll('strong');
+  boldWords.forEach(word => {
+    word.style.color = 'rgb(0, 128, 0)'; // green
+  });
+}
 
 
 }
@@ -7,6 +12,9 @@ function highlight() {
 
 function return_normal() {
     //Write your code here
-
+const boldWords = document.querySelectorAll('strong');
+  boldWords.forEach(word => {
+    word.style.color = 'rgb(0, 0, 0)'; // black
+  });
     
 }
